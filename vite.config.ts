a79d22d825d78701a4a9b8be7bc38404d22d9707
waleedehab07-sketch <waleedehab07@gmail.com>
2026-06-17 20:45:23 +0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        chart: 'chart.html',
+        profile: 'profile.html'
+      }
+    }
+  }
+})
