c5ac85b062f076430c38bdbf1ad3a1db238f958e
waleedehab07-sketch <waleedehab07@gmail.com>
2026-06-22 05:19:44 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        chart: 'chart.html',
-        profile: 'profile.html'
-        rules: 'rules.html'
+        main: resolve(__dirname, 'index.html'),
+        profile: resolve(__dirname, 'profile.html'),
+        chart: resolve(__dirname, 'chart.html'),
+        rules: resolve(__dirname, 'rules.html')
       }
     }
   }
-})
+});
